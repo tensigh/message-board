f74bf69ecac5928f27f7cace4309e8a9e71045c6
tensigh <kurisu@linuxdiaries.com>
2015-11-23 12:56:29 +0000

    Changed age to be numeric

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
 	validates :name, length: { maximum: 20 }, presence: true
 	validates :body, length: { minimum: 2, maximum: 30 }, presence: true
-	validates :age, length: { minimum: 1}, presence: true
+	validates :age, length: { minimum: 1, maximum: 3}, presence: true, numericality: { greater_than_or_equal_to: 1}
 end
